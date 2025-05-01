734a320b40cda34354943376b8f8acd396beee70
Tanvi <tanvi.33012@venturepact.com>
2025-05-01 13:19:50 +0530

    questions

--- form-builder/src/app/checkbox/checkbox.component.ts
+++ form-builder/src/app/checkbox/checkbox.component.ts
@@ -1,13 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component ,Input} from '@angular/core';
+import { Component ,Input,Output,EventEmitter} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checkbox',
   standalone:true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './checkbox.component.html',
   styleUrl: './checkbox.component.css'
 })
 export class CheckboxComponent {
   @Input() field: any;
+  isChecked: boolean = false; // Changed from answer to isChecked for clarity
+
+  @Output() answerSelected = new EventEmitter<{
+    question: string;
+    answer: string // Changed to boolean
+    answerText: string;
+    fieldType: string;
+  }>();
+
+  onInputChange() {
+    this.answerSelected.emit({
+      question: this.field.label || this.field.labelText,
+      answer: this.isChecked.toString(),
+      answerText: this.isChecked ? 'checked' : 'unchecked',
+      fieldType: 'checkbox'
+    });
+  }
 }
